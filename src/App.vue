9944500e733e264d35f0eb0c69d5610d47cfb878
苏子龙 <1500116008@qq.com>
2021-03-12 09:05:00 +0800

    come on

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <h1>老子终于成功了</h1>
+    <h1>给我发邮件呀呀呀呀呀</h1>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
